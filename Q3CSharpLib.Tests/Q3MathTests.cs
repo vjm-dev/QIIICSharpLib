51ebed9b261cd3d000731b5f280e155e9b12c5b5
vjm-dev <vjaume@outlook.com>
2025-05-30 19:16:08 +0200

    Add xUnit tests

---
+++ Q3CSharpLib.Tests/Q3MathTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using Q3CSharpLib;
+
+namespace Q3CSharpLib.Tests
+{
+    public class Q3MathTests
+    {
+        [Fact]
+        public void Q_rsqrt_ExpectedValue()
+        {
+            float input = 42f;
+            float expected = 0.15403557f;
+            float actual = QMath.Q_rsqrt(input);
+            Assert.Equal(expected, actual, 5); // 5 decimals precision
+        }
+    }
+}
---
+++ Q3CSharpLib.Tests/Q3StringTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Q3CSharpLib;
+using System.Text;
+
+namespace Q3CSharpLib.Tests
+{
+    public class Q3StringTests
+    {
+        [Fact]
+        public void Q_vsprintf_ExpectedValue()
+        {
+            string input = "Inverse fast";
+            float input2 = QMath.Q_rsqrt(42);
+            string expected = "Inverse fast square root calculated: 0.15403557";
+
+            var sb = new StringBuilder();
+            QLib.Q_vsprintf(sb, "%s square root calculated: %.8f", new object[] { input, input2 });
+
+            // QShared.Com_Printf(sb.ToString()); // "Inverse fast square root calculated: 0.15403557"
+            string actual = sb.ToString();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
